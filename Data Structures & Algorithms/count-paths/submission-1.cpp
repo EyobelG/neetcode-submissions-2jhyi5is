feb69607481219604500700e1e15148e72fd81fe
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-04-05 15:18:56 -0400

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> memo(m+1, vector<int>(n + 1, -1));
+        return solve(m, n, memo);
+    }
+private: 
+    int solve(int m, int n, vector<vector<int>>& memo) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        if (memo[m][n] != -1) {
+            return memo[m][n];
+        }
+        
+        return memo[m][n] = solve(m - 1, n, memo) + solve(m, n - 1, memo);
+
+    }
+   
+};
