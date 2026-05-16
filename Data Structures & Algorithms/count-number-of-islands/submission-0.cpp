98a81bd3b5175cbd6722038f03ad149be73b3c97
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-05-16 16:19:24 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+// 1 is land and 0 is water. If land is surrounded by water, there is an island (h/v)
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty() || grid[0].empty()) {
+            return 0;
+        }
+        
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int islands = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') {
+                    islands++;
+                    dfs(grid, r, c, rows, cols);
+                }
+            }
+        }
+        return islands;
+    }
+private:
+    void dfs(vector<vector<char>>& grid, int r, int c, int rows, int cols) {
+        if (r < 0 ||r >= rows || c < 0 || c >= cols || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+
+        dfs(grid, r + 1, c, rows, cols);
+        dfs(grid, r - 1, c, rows, cols);
+        dfs(grid, r, c + 1, rows, cols);
+        dfs(grid, r, c - 1, rows, cols);
+    }
+};
