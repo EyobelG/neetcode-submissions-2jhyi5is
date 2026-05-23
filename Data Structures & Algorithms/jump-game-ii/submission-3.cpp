573b5ade34b21ee7b6df44f8b581c7bd36e32db2
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-05-23 16:42:57 -0400

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        // Entrance
+        int jumps = 0;
+        int l = 0;
+        int r = 0;
+
+        // Guard Room
+        while (r < nums.size() - 1){
+            // Lookout tower
+            int farthest = 0;
+            // Scanning
+            for (int i = l; i <= r; i++) {
+                // Treasure room
+                farthest = max(farthest, i + nums[i]);
+            }
+
+            // Bridge of boundaries
+            l = r + 1;
+            r = farthest;
+            jumps++;
+
+            // Checkpoint Hall
+
+        }
+        return jumps;
+
+
+        // Throne Room
+    }
+};
