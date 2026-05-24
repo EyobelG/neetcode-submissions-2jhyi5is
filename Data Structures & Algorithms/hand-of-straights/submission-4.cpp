1f36d6083a5e612e58ceb282d2b50f8390dbac31
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-05-24 01:20:00 -0400

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        
+        // WIZARD
+        // Whole Check, Inventory, Assemble, Repeat and Reduce, Done
+
+        if (hand.size() % groupSize != 0) {
+            return false;
+        }
+
+        unordered_map <int, int> freq;
+    
+
+        for (int card : hand) {
+            freq[card]++;
+        }
+        
+        sort(hand.begin(), hand.end());
+
+        for (int card: hand) {
+            if (freq[card] == 0) continue;
+
+            for (int i = 0; i < groupSize; i++) {
+                int current = card + i;
+
+                if (freq[current] == 0) {
+                    return false;
+                    
+                }
+                freq[current]--;
+               
+            }
+        
+         
+        }
+        return true;
+        
+    }
+};
