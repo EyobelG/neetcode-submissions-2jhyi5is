26e88f8b47051e0f00c112ab5015b4aabc52f7b3
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-05-26 15:31:06 -0400

    Add: insert-new-interval - submission-5

---
+++ Data Structures & Algorithms/insert-new-interval/submission-5.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        if (intervals.empty()) return {newInterval};
+
+        int i = 0;
+        int n = intervals.size();
+
+        int newStart = newInterval[0];
+        int newEnd = newInterval[1];
+
+        vector<vector<int>> result;
+
+        result.reserve(n + 1);
+
+        while (i < n && intervals[i][1] < newStart) {
+            result.push_back(std::move(intervals[i]));
+            i++;
+        }
+
+        while (i < n && intervals[i][0] <= newEnd) {
+            newStart = min(newStart, intervals[i][0]);
+            newEnd = max(newEnd, intervals[i][1]);
+            i++;
+        }
+        result.emplace_back(vector<int>{newStart, newEnd});
+
+        while (i < n) {
+            result.push_back(std::move(intervals[i]));
+            i++;
+        }
+
+        return result;
+
+    }
+};
