fd94cee876b4614eb50aae3b48cdcf242e11dfd0
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-06-01 17:19:11 -0400

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,69 @@
+from queue import deque
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        if not grid:
+            return None
+        
+        INF = (2 ** 31) - 1
+      
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        q = deque()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    visited.add((r, c))
+                    q.append((r, c))
+        
+        while q:
+            r, c = q.popleft()
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if (0 <= nr < ROWS and 
+                    0 <= nc < COLS and
+                    (nr, nc) not in visited and
+                    grid[nr][nc] == INF):
+                        grid[nr][nc] = grid[r][c] + 1
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+
+
+        
+    
+        
+
+
+
+
+
+
+        """
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or
+                c >= COLS or grid[r][c] == "0"
+            ):
+                return
+
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
+
+        return islands
+        """
