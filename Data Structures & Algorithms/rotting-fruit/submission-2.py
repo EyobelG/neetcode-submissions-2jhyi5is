c96b5d339690270cfbfdb2dfca45c48fc5de949b
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-06-01 22:15:05 -0400

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,100 @@
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        minutes = 0
+        fresh = 0
+
+        q = deque()
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+                elif grid[r][c] == 2:
+                    q.append((r, c))
+
+      
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        while q and fresh > 0:
+            size = len(q)
+            for i in range(size):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+
+                    if (0 <= nr < rows and 0 <= nc < cols and
+                        grid[nr][nc] == 1):
+                        grid[nr][nc] = 2
+                        q.append((nr, nc))
+                        fresh -= 1
+            minutes += 1
+        if fresh == 0:
+            return minutes
+        else:
+            return -1
+
+
+
+                    
+                
+
+
+
+
+
+
+        """
+
+        class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int minutes = 0;
+        int fresh = 0;
+
+        queue<pair<int, int>> q;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    fresh++;
+                } else if (grid[r][c] == 2) {
+                    q.push({r, c});
+                }
+            }
+        }    
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, 
+        {1, 0}, {-1, 0}};
+
+        while (!q.empty() && fresh > 0) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                pair<int, int> current = q.front();
+                q.pop();
+                int r = current.first;
+                int c = current.second;
+
+                for (auto& dir : directions) {
+                    int nr = r + dir.first;
+                    int nc = c + dir.second;
+
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols &&
+                        grid[nr][nc] == 1) {
+                            grid[nr][nc] = 2;
+                            q.push({nr, nc});
+                            fresh--;
+                        }
+                }
+            }
+            minutes++;
+        }    
+        return (fresh == 0) ? minutes : -1;
+    }
+};
+
+
+
+        """
