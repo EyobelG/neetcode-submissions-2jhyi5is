37491efbaa07061c6c6e442661f6a077e3d23233
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-06-02 10:57:34 -0400

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,104 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board:
+            return None
+        ROWS, COLS = len(board), len(board[0])
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS 
+                or c >= COLS or board[r][c] != "O"):
+                return
+            board[r][c] = "#"
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        for c in range(COLS):
+            dfs(0, c)
+            dfs(ROWS - 1, c)
+        for r in range(ROWS):
+            dfs(r, 0)
+            dfs(r, COLS - 1)
+      
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "#":
+                    board[r][c] = "O"
+
+
+        
+        
+
+
+
+
+
+"""
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (r < 0 or r == ROWS or c < 0 or
+                c == COLS or grid[r][c] == 0 or
+                (r, c) in visit
+            ):
+                return 0
+            visit.add((r, c))
+            return (1 + dfs(r + 1, c) +
+                        dfs(r - 1, c) +
+                        dfs(r, c + 1) +
+                        dfs(r, c - 1))
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights:
+            return []
+        pacific = set()
+        atlantic = set()
+
+        ROWS, COLS = len(heights), len(heights[0])
+
+        def dfs(r, c, visited, prev_height):
+            if (r < 0 or c < 0 or r >= ROWS or
+                c >= COLS or (r, c) in visited or heights[r][c] < prev_height):
+                return
+            visited.add((r, c))
+
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+
+        for c in range(COLS):
+            dfs(0, c, pacific, heights[0][c])
+        for r in range (ROWS):
+            dfs(r, 0, pacific, heights[r][0])
+        
+        for c in range(COLS):
+            dfs(ROWS - 1, c, atlantic, heights[ROWS - 1][c])
+        for r in range (ROWS):
+            dfs(r, COLS - 1, atlantic, heights[r][COLS - 1])
+
+        res = []
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    res.append([r, c])
+        
+        return res
+    
+"""
