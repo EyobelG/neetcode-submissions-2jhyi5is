c562faf0e976d692a9a2a3f675aa67f64de8c737
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-06-03 14:02:07 -0400

    Add: course-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-7.py
@@ -0,0 +1,70 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        preMap = { i:[] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        res = []
+        visitSet = set()
+        cycleSet = set()
+
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if crs in cycleSet:
+                return True
+            visitSet.add(crs)
+
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visitSet.remove(crs)
+            cycleSet.add(crs)
+            res.append(crs)
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return []
+        return res
+        
+
+
+
+        """
+
+        # Use topological sort
+from collections import deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = { i:[] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visitSet = set()
+
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if preMap[crs] == []:
+                return True
+            
+            visitSet.add(crs)
+
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            visitSet.remove(crs)
+
+            preMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
+        """
