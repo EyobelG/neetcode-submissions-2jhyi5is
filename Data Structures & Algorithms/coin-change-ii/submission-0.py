15e558aa21400f1a4588984add255f19d2fb33ec
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-06-07 15:58:25 -0400

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+        return dp[amount]
+
+
+
+
+"""
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                if dp[i - coin] != amount + 1:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return -1 if dp[amount] > amount else dp[amount]
+        
+        
+
+        
+"""
