2abb8195a63db4f5b6f2deb6f798e616821eaa6e
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-06-19 13:44:22 -0400

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
@@ -0,0 +1,44 @@
+import math
+import heapq
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        maxHeap = []
+        for x, y in points:
+            dist = x**2 + y**2
+            heapq.heappush(maxHeap, (-dist, [x, y]))
+
+            if len(maxHeap) > k:
+                heapq.heappop(maxHeap)
+        return [point for dist, point in maxHeap]
+        
+        
+
+
+
+"""
+import heapq
+from typing import List
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minHeap = []
+
+        for num in nums:
+            heapq.heappush(self.minHeap, num)
+
+            if len(self.minHeap) > self.k:
+                heapq.heappop(self.minHeap)
+        
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        
+        return self.minHeap[0]
+        
+"""
