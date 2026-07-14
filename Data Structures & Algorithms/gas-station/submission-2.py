ae727e7ce35a2f10f70b575228397e5c9f0f6cba
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-07-14 18:35:00 -0400

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total = 0 # Running sum of every station
+        tank = 0 # Current fuel
+        start = 0 # Current best guess
+
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            tank += diff
+            total += diff
+
+            if tank < 0:
+                start = i + 1
+                tank = 0    
+        if total >= 0:
+            return start
+        else:
+            return -1
+
+
+'''
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+        int n = gas.size();
+
+        for (int i = 0; i < n; i++) {
+            int diff = gas[i] - cost[i];
+            tank += diff;
+            total += diff;
+            if (tank < 0){
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        if (total >= 0) {
+            return start;
+        } else {
+            return -1;
+        }
+        
+    }
+};
+'''
