d26c9e4455ec94b1a9e52a419cb16cd6d7fdce06
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-08-16 16:09:38 -0400

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1.0
+        
+        if n < 0:
+            x = 1 / x
+            n = -n
+        res = 1.0
+        cur_prod = x
+
+        while n > 0:
+            if n % 2 == 1:
+                res *= cur_prod
+            cur_prod *= cur_prod
+            n //= 2
+        return res
+        
