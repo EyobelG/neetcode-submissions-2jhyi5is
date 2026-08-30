b574dd179b21143f92ff761e6504fc84ce071026
Eyobel Gebre <eyobel.gebre@tufts.edu>
2026-08-30 14:30:55 -0400

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set <int> hashSet;
+        for (int num: nums) {
+            hashSet.insert(num);
+        }
+
+        int result = 0;
+        for (int num: hashSet) {
+            if (not hashSet.contains(num - 1)) {
+                int next = num;
+                while (hashSet.contains(next)) {
+                    next++;
+                }
+                result = std::max(result, next - num);
+            }
+        }
+        return result;
+
+        
+    }
+
+
+    
+};
